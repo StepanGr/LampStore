a21fe7c5870d3d5d3d046349af15d5358499a338
mato <mtmato6@gmail.com>
2014-04-11 17:09:09 +0300

    new project

---
+++ NewVersionLampstore/Binders/UserSessionDataBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewVersionLampstore.Models;
+
+namespace NewVersionLampstore.Binders
+{
+    public class UserSessionDataBinder : IModelBinder
+    {
+        public Object BindModel(ControllerContext controlerContext, ModelBindingContext bindingContext)
+        {
+            FilterData Filter = (FilterData)controlerContext.HttpContext.Session[NewVersionLampstore.Constants.SESSION_FILTER];
+            if (Filter == null)
+            {
+                Filter = new FilterData();
+                controlerContext.HttpContext.Session[NewVersionLampstore.Constants.SESSION_FILTER] = Filter;
+
+            }
+            
+           return Filter;
+        }
+    }
+}
